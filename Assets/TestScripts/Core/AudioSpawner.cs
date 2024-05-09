9e534fdaf899e4091f624a2742bfa89a9d64d670
VMware233 <460830558qq@gmail.com>
2024-05-09 12:12:47 +0800

    Update

---
+++ Assets/TestScripts/Core/AudioSpawner.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Triggers;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using VMFramework.Core;
+
+namespace Demo
+{
+    public class AudioSpawner : UniqueMonoBehaviour<AudioSpawner>
+    {
+        #region Config
+
+        /// <summary>
+        /// AudioSource组件的默认容器
+        /// </summary>
+        [SerializeField]
+        private Transform audioSourceDefaultContainer;
+
+        #endregion
+
+        // 音频对象池
+        [ShowInInspector]
+        private static Dictionary<AudioClip, IComponentPool<AudioSource>>
+            audioSourcePoolDictionary = new();
+
+        /// <summary>
+        /// 播放音效
+        /// </summary>
+        /// <param name="audioClip"></param>
+        /// <param name="position">AudioSource组件所在的位置</param>
+        /// <param name="autoCheckStop">是否在音频播放结束时自动Return给对象池</param>
+        /// <param name="parent">AudioSource组件的父Transform</param>
+        /// <returns></returns>
+        [Button]
+        public static AudioSource Play(AudioClip audioClip, Vector3 position,
+            bool autoCheckStop, Transform parent = null)
+        {
+            if (audioClip == null)
+            {
+                Debug.LogError("AudioClip is null");
+                return null;
+            }
+
+            var container = parent != null
+                ? parent
+                : instance.audioSourceDefaultContainer;
+
+            if (audioSourcePoolDictionary.TryGetValue(audioClip,
+                    out var audioSourcePool) == false)
+            {
+                audioSourcePool = new ComponentStackPool<AudioSource>();
+                audioSourcePoolDictionary.Add(audioClip, audioSourcePool);
+            }
+
+            var audioSource =
+                audioSourcePool.Get(container, false, out var isFreshlyCreated);
+
+            if (isFreshlyCreated)
+            {
+                audioSource.clip = audioClip;
+            }
+
+            audioSource.transform.position = position;
+            audioSource.loop = false;
+
+            audioSource.Play();
+
+            if (autoCheckStop)
+            {
+                _ = CheckStop(audioSource);
+            }
+
+            return audioSource;
+        }
+
+        /// <summary>
+        /// 回收音效
+        /// </summary>
+        /// <param name="audioSource"></param>
+        [Button]
+        public static void Return(AudioSource audioSource)
+        {
+            audioSource.Stop();
+
+            if (audioSource.gameObject.activeSelf)
+            {
+                audioSource.transform.SetParent(instance
+                    .audioSourceDefaultContainer);
+
+                if (audioSource.clip == null)
+                {
+                    Debug.LogError("AudioSource.clip is null");
+                    return;
+                }
+
+                if (audioSourcePoolDictionary.TryGetValue(audioSource.clip,
+                        out var audioSourcePool) == false)
+                {
+                    audioSourcePool = new ComponentStackPool<AudioSource>();
+                    audioSourcePoolDictionary.Add(audioSource.clip, audioSourcePool);
+                }
+
+                audioSourcePool.Return(audioSource);
+            }
+        }
+
+        /// <summary>
+        /// 检查音频是否播放结束，如果结束则回收
+        /// </summary>
+        /// <param name="audioSource"></param>
+        /// <returns></returns>
+        private static async UniTaskVoid CheckStop(AudioSource audioSource)
+        {
+            await UniTask.WaitUntil(() => audioSource.isPlaying == false);
+
+            Return(audioSource);
+        }
+    }
+}
---
+++ Assets/TestScripts/Core/KSet/RectangleIntegerDemo.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using VMFramework.Core;
+
+public class RectangleIntegerDemo : MonoBehaviour
+{
+    void Start()
+    {
+        // 创建一个[0, 2]x[0, 3]的整数矩形，用尺寸初始化
+        // 这里乘号是笛卡尔积，相当于一个从坐标(0, 0)到(2, 3)的矩形
+        var a = new RectangleInteger(new Vector2Int(3, 4));
+
+        // 创建一个[2, 3]x[5, 6]的整数矩形，用最小值和最大值初始化
+        var b = new RectangleInteger(new Vector2Int(2, 3), new Vector2Int(5, 6));
+
+        // 复制一个整数矩形
+        var c = new RectangleInteger(a);
+
+        // 用RectangleInteger.unit快速创建一个[0, 1]x[0, 1]的整数矩形
+        var d = RectangleInteger.unit;
+
+        // [0, 2]x[0, 3]与(3, 4)相加，得到一个[3, 6]x[3, 7]的整数矩形
+        var e = a + new Vector2Int(3, 4);
+
+        // [3, 6]x[3, 7]加负号，得到一个[-6, -3]x[-7, -3]的整数矩形
+        // 与相对于坐标原点作镜像一样
+        var f = -e;
+
+        // 创建一个[0, 2]x[0, 3]的整数矩形，即从坐标(0, 0)到(2, 3)的矩形
+        var rangeX = RangeInteger.unit * 2;
+        var rangeY = RangeInteger.unit * 3;
+        var rectangle = rangeX * rangeY;
+
+        // 获取[0, 2]x[0, 3]这个矩形上所有边界上的整数点
+        foreach (var point in rectangle.GetAllBoundaryPoints())
+        {
+            Debug.Log(point);
+        }
+
+        // 获取[0, 2]x[0, 3]这个矩形内的所有整数点（包括边界）
+        foreach (var point in rectangle.GetAllPoints())
+        {
+            Debug.Log(point);
+        }
+
+        // 从[0, 2]x[0, 3]这个矩形内随机取5个不同的随机点（包括边界）
+        foreach (var point in rectangle.GetRandomUniquePoints(5))
+        {
+            Debug.Log(point);
+        }
+    }
+}
