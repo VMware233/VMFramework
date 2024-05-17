4bc51e7de989b60cd8c56467e9ffa96df588f285
VMware233 <460830558qq@gmail.com>
2024-05-17 13:58:17 +0800

    Example

---
+++ Assets/Examples/GameEvents/ColliderMouseEventDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using VMFramework.GameEvents;
+using VMFramework.Procedure;
+
+namespace VMFramework.Examples
+{
+    [ManagerCreationProvider("Demo")]
+    public class ColliderMouseEventDemo : ManagerBehaviour<ColliderMouseEventDemo>, IManagerBehaviour
+    {
+        void IInitializer.OnInitComplete(Action onDone)
+        {
+            ColliderMouseEventManager.AddCallback(MouseEventType.PointerEnter, OnPointerEnter);
+            ColliderMouseEventManager.AddCallback(MouseEventType.PointerLeave, OnPointerLeave);
+        }
+
+        private void OnPointerEnter(ColliderMouseEvent gameEvent)
+        {
+            Debug.Log("Pointer Entered: " + gameEvent.trigger.name);
+        }
+        
+        private void OnPointerLeave(ColliderMouseEvent gameEvent)
+        {
+            Debug.Log("Pointer Left: " + gameEvent.trigger.name);
+        }
+    }
+}
