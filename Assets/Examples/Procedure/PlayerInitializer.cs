4e6982cfdc5e5e3b78a1ced198901eabbb52be50
VMware233 <460830558qq@gmail.com>
2024-06-20 19:49:13 +0800

    Player Initialize Example

---
+++ Assets/Examples/Procedure/PlayerInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using VMFramework.Procedure;
+
+namespace VMFramework.Examples
+{
+    [GameInitializerRegister(GameInitializationDoneProcedure.ID, ProcedureLoadingType.OnEnter)]
+    public sealed class PlayerInitializer : IGameInitializer
+    {
+        void IInitializer.OnPostInit(Action onDone)
+        {
+            // Do something with the player here.
+            
+            // Call the onDone callback to signal that the initialization is done.
+            onDone();
+        }
+    }
+}
